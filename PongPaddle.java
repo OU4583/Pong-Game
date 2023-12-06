886775122549c80eba04f4cc1a7de362b594e37d
OU4583 <99269225+OU4583@users.noreply.github.com>
2023-12-06 10:08:56 -0500

    Create PongPaddle.java

---
+++ PongPaddle.java
@@ -0,0 +1,46 @@
+public class PongPaddle {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    public PongPaddle(int top, int left, int w, int h)
+    {
+        width = w;
+        height = h;
+        x = left;
+        y = top;
+    }
+    public int getBottomY()
+    {
+        return height + y - 1;
+
+    }
+    public int getLeftX()
+    {
+        return x;
+        
+    }
+    public int getRightX()
+    {
+        return x + width - 1;
+        
+    }
+    public int getTopY()
+    {
+        return y;
+        
+    }
+    public void moveDown(int d)
+    {
+        
+        y += d;
+        
+    }
+    public void moveUp(int d)
+    {
+        y -= d;
+
+    }
+
+    
+}
